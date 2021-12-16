d018a15dfc49dfa05c04eeb895243891c43f1273
Kaique Lira <Kaiqueliira@gmail.com>
2021-12-16 00:01:05 -0300

    criação de metodos get e set do localstorage

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ const Utils = {
         document.querySelector(".menu").classList.toggle('toggle')
     }
 }
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem('controle-financeiro:transactions')) || []
+    },
+    set(transactions) {
+        localStorage.setItem('controle-financeiro:transactions', JSON.stringify(transactions))
+    }
+}
+
+const Transaction = {
+    all: Storage.get(),
+    add(transaction) {
+        Transaction.all.push(transaction)
+    }
+}
+const DOM = {
+    innerHtmlTransaction(transaction) {
+        const html = `
+        <tr>
+        <td>+</td>
+        <td> Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+        <td>R$ 12.999,99 </td>
+        </tr>`
+    },
+
+}
